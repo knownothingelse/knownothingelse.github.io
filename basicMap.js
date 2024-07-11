62ecf2d0b207703a6c824603696a4dc3b02843be
knownothingelse <ishmaelaadel1@gmail.com>
2024-07-11 14:02:11 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -1,1 +1,12 @@
 console.log("JS" File Was loaded");
+
+// 35.217665 N, 80.831473 -> 35.217665, -80.831473
+
+let map = L.map("map", {
+center: [35.217665, -80.831473],
+zoom: 8
+}};
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
